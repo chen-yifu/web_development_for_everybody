d86b97540bb8528a62ac818d13e1124a3b88cc5b
Yifu Chen <yifu.chen@alumni.ubc.ca>
2020-01-06 17:36:28 -0800

    a3

---
+++ wd4e_course_1/a3/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Yifu Chen's PHP</title>
+  </head>
+  <body>
+<h1>Yifu Chen's PHP</h1>
+<pre>    Yifu</pre>
+<?php
+print hash('sha256', 'Yifu Chen');
+?>
+<a href="check.php">check error</a>
+<a href="fail.php">traceback</a>
+  </body>
+</html>

de331990e65990af1b97164bac8eef2975da5ec5
Yifu Chen <yifu.chen@alumni.ubc.ca>
2020-01-10 09:37:44 -0800

    completed

---
+++ wd4e_course_4/auto/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to View Automobiles Yifu Chen (Charles) 6d4e59d5</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<title>Welcome to View Automobiles Yifu Chen (Charles) 6d4e59d5</title>
+
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+
+<p>
+<a href="http://www.wa4e.com/code/rps.zip"
+ target="_blank">Source Code for this Application</a>
+</p>
+</div>
+</body>
